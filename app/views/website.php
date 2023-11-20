8b4aafbf79eebf8f69fa3e08b9459afb780d402c
NaufalDhafin <naufal280905@gmail.com>
2023-11-20 17:31:40 +0700

    'menambahkan'

--- app/views/website.php
+++ app/views/website.php
@@ -1,21 +1,20 @@
 <div class="website">
-
     <p class="page">Website</p>
-
     <div class="list">
-
         <div class="listW">
-
             <img src="../app/img/mvlcloud.png">
-
             <p class="nama">MVL Cloud</p>
-
             <p class="ket">Peyimpanan folder/file</p>
-
             <a href="../cloud/">Buka Website</a>
-
         </div>
-
     </div>
 
+    <div class="list">
+        <div class="listW">
+            <img src="../app/img/mvlcloud.png">
+            <p class="nama">CA Chat Anonimus</p>
+            <p class="ket">CA (Chat Anonimus), adalah pesan yang dikirim secara privat ke pengguna.</p>
+            <a href="msg/">Buka Website</a>
+        </div>
+    </div>
 </div>
